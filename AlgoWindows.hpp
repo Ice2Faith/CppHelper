a54bfe2f4777bcc61aa5e8075d9a9848b21a5059
Ice2Faith <45895088+Ice2Faith@users.noreply.github.com>
2020-11-22 14:23:58 +0800

    1st upload source code

---
+++ AlgoWindows.hpp
@@ -0,0 +1,28 @@
+#ifndef _ALGO_WINDOWS_HPP_
+#define _ALGO_WINDOWS_HPP_
+
+#include"AlgoWinFile.hpp"
+
+#include"AlgoWinDirectory.hpp"
+
+#include"AlgoWinString.hpp"
+
+#include"AlgoWinRegister.hpp"
+
+#include"AlgoWinService.hpp"
+
+#include"AlgoWinProcess.hpp"
+
+#include"AlgoWinThread.hpp"
+
+#include"AlgoWinODBC.hpp"
+
+#include"AlgoWinControl.hpp"
+
+#include"AlgoWinWindow.hpp"
+
+#include"AlgoWinBatchDraw.hpp"
+
+#include"AlgoWinPainter.hpp"
+
+#endif // _ALGO_WINDOWS_HPP_
